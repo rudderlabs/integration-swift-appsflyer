29f6a7dabcbc070d6f8d8d5ec0ece21dac794cd9
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-25 14:47:12 +0530

    chore: add appsflyer and rudder sdk swift deps
    Scanned-by: gitleaks 8.29.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "integration-swift-appsflyer",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +18,20 @@ let package = Package(
             targets: ["RudderIntegrationAppsFlyer"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static", .upToNextMajor(from: "6.17.0")),
+        // TODO: Update rudder-sdk-swift dependency after the stable release of rudder-sdk-swift.
+        .package(url: "https://github.com/rudderlabs/rudder-sdk-swift.git", branch: "feat/sdk-502-make-standard-integration-public")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RudderIntegrationAppsFlyer"
+            name: "RudderIntegrationAppsFlyer",
+            dependencies: [
+                .product(name: "AppsFlyerLib-Static", package: "AppsFlyerFramework-Static"),
+                .product(name: "RudderStackAnalytics", package: "rudder-sdk-swift")
+            ]
         ),
         .testTarget(
             name: "RudderIntegrationAppsFlyerTests",
